8bf8f861a2a87818ae0ac7708475165662e20641
Christian Ortiz <c.ortiz313@gmail.com>
2022-08-22 20:53:31 -0400

    Saved

--- TheBugTracker/Models/Ticket.cs
+++ TheBugTracker/Models/Ticket.cs
@@ -1,9 +1,24 @@
 ﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace TheBugTracker.Models
 {
     public class Ticket
     {
-        
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [DisplayName("Title")]
+        public string Title { get; set; }
+
+        [Required]
+        [DisplayName("Description")]
+        public string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayName("")]
     }
 }
 
