ee9ca5a679649925e9243cd5c282597c3fedc91c
Christian Ortiz <c.ortiz313@gmail.com>
2022-08-25 12:24:32 -0400

    Added BTCompanyInfo Interface and Class
    Implemented the interface and began implementing the class. requires more work to complete the methods

---
+++ TheBugTracker/Services/BTCompanyInfoService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TheBugTracker.Data;
+using TheBugTracker.Models;
+using TheBugTracker.Services.Interfaces;
+
+namespace TheBugTracker.Services
+{
+    public class BTCompanyInfoService : IBTCompanyInfoService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BTCompanyInfoService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<BTUser>> GetAllMembersAsync(int companyId)
+        {
+            List<BTUser> result = new();
+
+            result = await _context.Users.Where(u => u.CompanyId == companyId).ToListAsync();
+
+            return result;
+        }
+
+        public Task<List<Project>> GetAllProjectsAsync(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetAllTicketsAsync(int companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Company> GetCompanyInfoByIdAsync(int? company)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
