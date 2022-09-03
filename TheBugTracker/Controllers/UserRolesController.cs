b3e689da72de8658347320bfd44341a1551bd49e
Christian Ortiz <c.ortiz313@gmail.com>
2022-09-02 22:03:50 -0400

    Almost done manage user roles
    Still need to address database issue on run. Continue on PC

--- TheBugTracker/Controllers/UserRolesController.cs
+++ TheBugTracker/Controllers/UserRolesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using TheBugTracker.Extensions;
 using TheBugTracker.Models;
@@ -7,6 +8,8 @@ using TheBugTracker.Services.Interfaces;
 
 namespace TheBugTracker.Controllers
 {
+
+    [Authorize] // will be bounced to back to login page if not logged in
     public class UserRolesController : Controller
     {
         private readonly IBTRolesService _rolesService;
@@ -50,6 +53,39 @@ namespace TheBugTracker.Controllers
 
             // Return the model to the View
             return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken] // prevents cross-platform submissions
+        public async Task<IActionResult> ManageUserRoles(ManageUserRolesViewModel member)
+        {
+            // Get the company ID
+            int companyId = User.Identity.GetCompanyId().Value; // get company ID from logged in user, since they will be same company
+
+            // Instantiate the BTUser
+            BTUser btUser = (await _companyInfoService.GetAllMembersAsync(companyId)).FirstOrDefault(u => u.Id == member.BTUser.Id);
+
+
+            // Get Roles for the User
+            IEnumerable<string> roles = await _rolesService.GetUserRolesAsync(btUser);
+
+            // Grab the selected role
+            string userRole = member.SelectedRoles.FirstOrDefault();
+
+            if (!string.IsNullOrEmpty(userRole))
+            {
+                // Remove User from their roles
+                if(await _rolesService.RemoveUserFromRolesAsync(btUser, roles)) // if removing roles was succesful, add them to new role
+                {
+                    // Add user to the new role
+                    await _rolesService.AddUserToRoleAsync(btUser, userRole);
+                }
+
+            }
+
+            // Navigate back to the View
+            return RedirectToAction(nameof(ManageUserRoles)); // navigate right back to this page, the get method, to show results
+
         }
     }
 }
